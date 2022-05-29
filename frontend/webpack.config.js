3c61e4fb25c7eaf607c19357944a319019c88c68
15ice <cherry051725@gmail.com>
2022-05-29 21:11:49 +0900

    frontend init

---
+++ frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const environment = process.env.NODE_ENV || 'development';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'js/app': ['./src/App.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      AppConfig: path.join(__dirname, `/src/config/_${environment}.js`)
+    }
+  },
+};
