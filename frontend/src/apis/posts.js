22a7310f01313321824f008aa8999a0ef08ea0c5
15ice <cherry051725@gmail.com>
2022-06-11 17:58:39 +0900

    画面から表示/非表示を切り替えられるようにする

--- frontend/src/apis/posts.js
+++ frontend/src/apis/posts.js
@@ -1,9 +1,31 @@
 import axios from 'axios';
-import { postsIndexUrl } from './urls'
+import {
+  postsIndexUrl,
+  postsUpdateDisplayUrl,
+  postsUpdateHiddenUrl
+} from './urls'
 
-export const fetchPosts = (category, skip, take) => {
+export const fetchPosts = (params) => {
   return axios.get(
-    postsIndexUrl(category, skip, take),
+    postsIndexUrl(params.category, params.skip, params.take),
+    { withCredentials: true }
+  ).then(res => {
+    return res.data
+  }).catch((e) => { throw e; });
+}
+
+export const updatePostDisplay = (params) => {
+  return axios.put(
+    postsUpdateDisplayUrl(params.id),
+    { withCredentials: true }
+  ).then(res => {
+    return res.data
+  }).catch((e) => { throw e; });
+}
+
+export const updatePostHidden = (params) => {
+  return axios.put(
+    postsUpdateHiddenUrl(params.id),
     { withCredentials: true }
   ).then(res => {
     return res.data
--- frontend/src/containers/Mng/MngPosts.jsx
+++ frontend/src/containers/Mng/MngPosts.jsx
@@ -2,12 +2,16 @@ import React, { Fragment, useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Table, Popconfirm, message } from 'antd';
 import { format } from 'date-fns';
+import { AiFillEyeInvisible, AiOutlineEye } from 'react-icons/ai';
+import styled from 'styled-components';
 
 import SelectItem from '../../components/SelectItem.jsx';
 
 // api
 import {
   fetchPosts,
+  updatePostDisplay,
+  updatePostHidden
 } from '../../apis/posts';
 import {
   fetchCategories,
@@ -15,25 +19,31 @@ import {
 // constants
 import { HTTP_STATUS_CODE, NUM_OF_TAKE_POSTS } from '../../constants';
 
+const HiddenIcon = styled.span`
+  font-size: 1.3rem;
+`
+
 const MngPosts = () => {
   const [posts, setPosts] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [selectCategory, setSelectCategory] = useState(1);
   const navigate = useNavigate();
 
   useEffect(() => {
     getCategories();
+    getPosts(selectCategory, 0);
   }, []);
 
-  const postFormat = (id, user_id, user_name, mail, title, detail, is_hidden, created_at) => {
+  const postFormat = (id, userId, userName, mail, title, detail, isHidden, createdAt) => {
     return {
       'key': id,
-      'user_id': user_id,
-      'user_name': user_name,
+      'userId': userId,
+      'userName': userName,
       'mail': mail,
       'title': title,
       'detail': detail,
-      'is_hidden': is_hidden,
-      'created_at': created_at
+      'isHidden': isHidden,
+      'createdAt': createdAt
     }
   }
 
@@ -52,7 +62,11 @@ const MngPosts = () => {
   }
 
   const getPosts = (category, skip) => {
-    fetchPosts(category, skip, NUM_OF_TAKE_POSTS).then((res) => {
+    fetchPosts({
+      category: category,
+      skip: skip,
+      take: NUM_OF_TAKE_POSTS
+    }).then((res) => {
       var posts = res.map(r => postFormat(
         r.id,
         r.user_id,
@@ -69,20 +83,43 @@ const MngPosts = () => {
   }
 
   const handleSelectCategory = (value) => {
+    setSelectCategory(value);
     getPosts(value, 0);
   };
+
+  const handlePostDisplay = (id) => {
+    updatePostDisplay({
+      id: id
+    }).then((res) => {
+      getPosts(selectCategory, 0);
+      message.info('投稿を表示しました。');
+    }).catch((e) => {
+      console.error(e);
+    });
+  }
+
+  const handlePostHidden = (id) => {
+    updatePostHidden({
+      id: id
+    }).then((res) => {
+      getPosts(selectCategory, 0);
+      message.info('投稿を非表示にしました。');
+    }).catch((e) => {
+      console.error(e);
+    });
+  }
 
   const columns = [
     {
       title: '投稿日',
-      dataIndex: 'created_at',
-      key: 'created_at',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
       render: (date) => <time>{format(new Date(date), "yyyy/MM/dd hh:mm")}</time>
     },
     {
       title: '名前',
-      dataIndex: 'user_name',
-      key: 'user_name',
+      dataIndex: 'userName',
+      key: 'userName',
     },
     {
       title: 'Email',
@@ -98,7 +135,21 @@ const MngPosts = () => {
       title: '詳細',
       dataIndex: 'detail',
       key: 'detail',
-    }
+    },
+    {
+      title: '表示/非表示',
+      dataIndex: 'isHidden',
+      render: (_, record) => (
+        record.isHidden ?
+          <Popconfirm title="表示しますか?" onConfirm={() => handlePostDisplay(record.key)}>
+            <a><HiddenIcon><AiFillEyeInvisible /></HiddenIcon></a>
+          </Popconfirm>
+          :
+          <Popconfirm title="非表示にしますか?" onConfirm={() => handlePostHidden(record.key)}>
+            <a><HiddenIcon><AiOutlineEye /></HiddenIcon></a>
+          </Popconfirm>
+      ),
+    },
   ];
 
   return (
