4fdb831cdcfc1bdba806dcd5ee4faba81139df2b
15ice <cherry051725@gmail.com>
2022-06-20 00:44:10 +0900

    ログイン画面のデザイン変更

--- frontend/src/containers/Login.jsx
+++ frontend/src/containers/Login.jsx
@@ -10,15 +10,41 @@ import {
 } from '../reducers/sessions';
 // constants
 import { HTTP_STATUS_CODE, LOGIN_STATE } from '../constants';
+import { COLORS } from '../style_constants';
 
 // styled
+const LoginArea = styled.div`
+  margin: auto;
+  width: 50vw;
+  background-color: ${COLORS.LIGHT_PRIMARY_BACK};
+  padding: 45px;
+`
+const LoginTitleText = styled.h1`
+  color: ${COLORS.TEXT};
+  text-shadow: 1px 2px 3px ${COLORS.DARK_PRIMARY_BACK};
+`
 const LoginForm = styled.form`
+  text-align: center;
 `
 const PasswordInput = styled.input`
+  outline: 0;
+  width: 100%;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  font-size: 1.1rem;
 `
 const LoginButton = styled.button`
+  width: 100%;
+  border: 0;
+  font-size: 1.1rem;
+  background: ${COLORS.DARK_PRIMARY_BACK};
+  color: ${COLORS.TEXT};
+  padding: 15px;
+  cursor: pointer;
 `
 const ErrorMessage = styled.div`
+  color: red;
 `
 
 const Login = (props) => {
@@ -33,7 +59,7 @@ const Login = (props) => {
       props.sessionsDispatch({ type: sessionsActionTypes.LOGIN });
       navigate("/mng");
     }).catch((e) => {
-      props.setLoginState(LOGIN_STATE.NOT_LOGIN);
+      props.sessionsDispatch({ type: sessionsActionTypes.LOGOUT });
       if (e.response.status === HTTP_STATUS_CODE.UNAUTHORIZED) {
         setErrorMsg("パスワードが違います。");
       } else {
@@ -45,22 +71,24 @@ const Login = (props) => {
 
   return (
     <Fragment>
-      <h1>Login</h1>
-      {
-        errorMsg ?
-          <ErrorMessage>
-            {errorMsg}
-          </ErrorMessage>
-          : ""
-      }
-      <LoginForm onSubmit={handleSubmit}>
-        <PasswordInput
-          type="password"
-          placeholder="password"
-          onChange={e => setPassword(e.target.value)}
-        />
-        <LoginButton>login</LoginButton>
-      </LoginForm>
+      <LoginArea>
+        <LoginTitleText>Admin</LoginTitleText>
+        {
+          errorMsg ?
+            <ErrorMessage>
+              {errorMsg}
+            </ErrorMessage>
+            : ""
+        }
+        <LoginForm onSubmit={handleSubmit}>
+          <PasswordInput
+            type="password"
+            placeholder="password"
+            onChange={e => setPassword(e.target.value)}
+          />
+          <LoginButton>LOGIN</LoginButton>
+        </LoginForm>
+      </LoginArea>
     </Fragment>
   )
 }
